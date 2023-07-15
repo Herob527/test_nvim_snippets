76e00e11683ac11edf5a3c33e3ff2943b18e316c
Szymon Wrzos <szymon.wrzos@bitapps.fi>
2023-07-15 10:34:56 +0200

    Initial commit

---
+++ console_snippet/test.jsx
@@ -0,0 +1,26 @@
+const arrow = () => {};
+
+function normal() {}
+
+const test = function () {};
+
+class Class {
+  constructor() {}
+
+  method() {}
+
+  static property = () => {};
+
+  static nested_func = () => {
+    const y = () => {};
+  };
+}
+const x = () => {
+  (() => {})();
+
+  if (2) {
+  } else {
+  }
+};
+
+const y = new Map();
